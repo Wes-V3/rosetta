f2646e94a97a0c7f815106c2fc330df6da0b6d14
sen <swei25@jhu.edu>
2024-01-29 17:27:12 -0600

    Loading pose from PDB file

--- source/src/apps/pilot/sen/bootcamp.cc
+++ source/src/apps/pilot/sen/bootcamp.cc
@@ -8,19 +8,25 @@
 // (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
 #include<iostream>
+
 #include<devel/init.hh> 
 #include<utility/vector1.hh>
 #include<basic/options/option.hh>
 #include<basic/options/keys/in.OptionKeys.gen.hh>
+
+#include<core/pose/Pose.fwd.hh>
+#include<utility/pointer/owning_ptr.hh>
+#include<core/pose/Pose.hh>
+#include<core/import_pose/import_pose.hh> //pose_from_file
 
 int main( int argc, char ** argv ) {
 	devel::init( argc, argv );
 	utility::vector1< std::string > filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ].value();
 	if ( filenames.size() > 0 ) {
 		std::cout << "You entered: " << filenames[1] << " as the PDB file to be read" << std::endl;
-
 	} else {
 		std::cout << "You didn't provide a PDB file with the -in::file::s option" << std::endl;
 		return 1;
 	}
+	core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
 }
